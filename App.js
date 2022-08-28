004a32428a4c714252e9d559b919875bc268158b
Jussi Junnila <74765606+koodiameles@users.noreply.github.com>
2022-08-28 15:19:51 +0300

    teht1 done

--- App.js
+++ App.js
@@ -1,21 +1,74 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Button, Alert, TextInput, Image, Text } from 'react-native';
 
 export default function App() {
+
+  const [number1, setNumber1] = useState("");
+  const [number2, setNumber2] = useState("");
+  const [result, setResult] = useState("");
+
+  const plus = () => { 
+    let summation = Number(number1) + Number(number2);
+    setResult(summation)
+  };
+  const minus = () => { 
+    let deduction = Number(number1) - Number(number2);
+    setResult(deduction)
+  };
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.containerHeader}>
+        <Text style={styles.assignemtHeaderText}>TEHT 1 LASKIN</Text>
+      </View>
+      <View style={styles.container}>
+        <Text style={{color:"white"}}>Result: {result}</Text>
+        <TextInput keyboardType="numeric" style={styles.input} onChangeText={setNumber1} value={number1}/>
+        <TextInput keyboardType="numeric" style={styles.input} onChangeText={setNumber2} value={number2}/>
+        <View style={{display: 'flex', flexDirection: 'row', margin: 10}}>
+          <View style={{flex: 1, marginHorizontal: 20}}>
+            <Button color="green" onPress={plus} title="+" />
+          </View>
+          <View style={{flex: 1, marginHorizontal: 20}}>
+            <Button color="red" onPress={minus} title="-" />
+          </View>
+        </View>
+        <StatusBar style="auto" />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  containerHeader: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    // color:"white",
+    // justifyContent: 'start',
+  },
+  image : {
+    width: 250,
+    height: 100
+  },
+  input : {
+    width:200  , 
+    borderColor: 'gray', 
+    borderWidth: 1,
+    margin: 5,
+    color:"white",
+  },
+  assignemtHeaderText: {
+    fontSize: 40,
+    color: "white"
+  }
 });
+ 
